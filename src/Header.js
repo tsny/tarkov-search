8a4b9ad6b0afd5ac2ce30ee9acef56c08e659604
tsny <Taylor.W.Snyder@centene.com>
2020-03-29 12:16:36 -0500

    init commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from "react";
+import "./Header.css";
+
+export default class Header extends PureComponent
+{
+  render()
+  {
+    return (
+      <header className="component-header">
+        Tarkov Search
+      </header>
+    );
+  }
+}
